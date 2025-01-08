a2fb1ebfaee67d121f1cefc267f4cd68638238a1
jkfletcher98 <jkfletcher@bsu.edu>
2025-01-08 17:07:19 +0000

    Added hello.py

---
+++ project1/hello.py
@@ -0,0 +1,18 @@
+userName = input("What is your name?")
+print(f"Hi, {userName}")
+if userName.upper == "ANDY":
+    print("I'm looking forward to this semester with you")
+
+keepGoing = True
+while keepGoing:
+    response = input("Do you think I'm going to do well in CS 121?")
+    if response.upper.startswith("Y"):
+        print("Thanks for the vote of confidence!")
+        keepGoing = False
+    elif response.upper.startswith("N"):
+        print("Honestly, that's fair.")
+        keepGoing = False
+    else:
+        print("I'm not sure I understand. Please answer 'yes' or 'no'.")
+
+print("Thanks for talking with me!")
